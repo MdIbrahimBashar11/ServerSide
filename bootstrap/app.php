0ce5f7145c0d19bdeb2619f3ca3a9669c80b65c8
MdIbrahimBashar11 <mdibrahimbashar11@gmail.com>
2026-05-18 11:16:02 +0600

    -

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         channels: __DIR__.'/../routes/channels.php',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->web(append: [
             \App\Http\Middleware\AffiliateMiddleware::class,
             \App\Http\Middleware\IdentifyProjectByDomain::class,
